2004429fb869c32657ca897e8093ab9ec772b7b0
Ksistof Vinco <ksistof.vinco@gmail.com>
2020-02-05 08:12:37 +0200

    redone database validated mails

--- plugins/GoogleAuth/GoogleAuth.php
+++ plugins/GoogleAuth/GoogleAuth.php
@@ -11,7 +11,7 @@ class GoogleAuth extends LMSPlugin
     const PLUGIN_NAME = 'GoogleAuth';
     const PLUGIN_DESCRIPTION = 'Enables google OAuth login to userpanel';
     const PLUGIN_AUTHOR = 'Kšištof Vinčo &lt;ksistof.vinco@gmail.com&gt;';
-    const PLUGIN_DBVERSION = '2020012303';
+    const PLUGIN_DBVERSION = '2020020401';
     const plugin_directory_name = 'GoogleAuth';
 
     public function registerHandlers()
---
+++ plugins/GoogleAuth/lib/upgradedb/postgres.2020020401.php
@@ -0,0 +1,28 @@
+<?php
+
+$this->BeginTrans();
+
+$exists = $this->GetOne("
+  SELECT EXISTS (SELECT 1 
+  FROM information_schema.tables 
+  WHERE table_name='contactvalidated')
+");
+
+
+if($exists != "t"){
+  $this->Execute("CREATE SEQUENCE contactvalidated_id_seq");
+  $this->Execute("
+    CREATE TABLE public.contactvalidated (
+      id integer DEFAULT nextval('contactvalidated_id_seq'::text) NOT NULL,
+      contact varchar(255) NOT NULL,
+      customer_id integer  NOT NULL,
+      validated boolean default false,
+      PRIMARY KEY (id),
+      UNIQUE (contact)
+    );
+  ");
+} 
+
+$this->Execute("UPDATE dbinfo SET keyvalue = '2020020401' WHERE keytype='dbversion_GoogleAuth'");
+
+$this->CommitTrans();
